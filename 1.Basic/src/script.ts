a9474128a25515877a92e9b152fed4edb5c1c13f
Shefat Al Mahmud <shefat2002@gmail.com>
2026-05-19 11:20:04 +0600

    added basic codes

--- 1.Basic/src/script.ts
+++ 1.Basic/src/script.ts
@@ -1,2 +1,42 @@
 const countryname = "Bangladesh";
 console.log(countryname);
+
+//----- Data Types -----
+// let, const
+
+// boolean
+let isActive: boolean;
+isActive = true;
+
+// number
+let age: number;
+age = 10;
+const largeNumber = BigInt(9999999999999999); // 2^53 - 1
+
+// string
+let color: string;
+let fullName = "Shefat";
+let sentence = `Hello, This is ${fullName}.`;
+
+// object
+const user = {
+  name: "Alice",
+  age: 20,
+  isAdmin: true,
+};
+
+// any
+let a; // by default any Type
+let b: any;
+
+b = 10;
+b = "Shefat";
+
+let c: (string|number|boolean)[] = []; // any type of array
+c.push(1);
+c.push("hello");
+c.push(true);
+
+c.forEach((element) => {
+  console.log(element);
+});
